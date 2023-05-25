f0ed84d07e9cdfc9dfce18becd9ceb10cbe72c35
omar-web-dev <qteam9035@gmail.com>
2023-05-25 10:45:32 +0600

    product save

--- app.js
+++ app.js
@@ -7,66 +7,23 @@ const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
 
-// schema design
-// const productSchema = mongoose.Schema({
-//   name: {
-//     type: String,
-//     required: [true, "Please provide a Product name"],
-//     trim: true,
-//     unique: true,
-//     minLength: [3, "Product name must be 3 characters"],
-//     maxLength: [20, "Product name Is to large"],
-//   },
-//   description: {
-//     type: String,
-//     required: [true, "Please don't empty product description"],
-//   },
-//   price: {
-//     type: Number,
-//     required: true,
-//     min: [true, " Price can be Negative"],
-//   },
-//   unit: {
-//     type: String,
-//     required: true,
-//     enum: {
-//       values : '["kg", "litter", "pcs"]',
-//       massage: "Unit value can't be {VALUE}, must be kg/litter/pcs",
-//     },
-//     quantity: {
-//       type: 0,
-//       required: true,
-//       min: [0, "quantity can't be Negative"],
-//       validate: {
-//         validator: (value) => {
-//           const integer = Number.isInteger(value);
-//           if (integer) {
-//             return true;
-//           } else {
-//             return false;
-//           }
-//         },
-//       },
-//       massage: "Product quantity can't be Integer number",
-//     },
-//     status: {
-//       type: String,
-//       required: true,
-//       enum: {
-//         values: ["in-stock", "out-of-stock", "discontinued"],
-//         massage: "Status can't be {VALUE",
-//       },
-//     },
-//   },
-//    timestamps: true,
-// });
+// Schema ➡ module ➡ query
+
+// {
+//   "name": "Rice",
+//   "description": "Rice is big price in markets",
+//   "price": 100,
+//   "unit": "kg",
+//   "quantity": 500,
+//   "status" : "in-stock"
+// }
 
 const productSchema = mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please provide a Product name"],
     trim: true,
-    unique: true,
+    unique: [true, "Have similar name product in your stock"],
     minLength: [3, "Product name must be 3 characters"],
     maxLength: [20, "Product name Is to large"],
   },
@@ -79,14 +36,14 @@ const productSchema = mongoose.Schema({
     required: true,
     min: [true, " Price can be Negative"],
   },
-  unit: {
-    type: String,
-    required: true,
-    enum: {
-      values: '["kg", "litter", "pcs"]',
-      massage: "Unit value can't be {VALUE}, must be kg/litter/pcs",
-    },
-  },
+  // unit: {
+  //   type: String,
+  //   required: true,
+  //   enum: {
+  //     values: '["kg", "litter", "pcs"]',
+  //     massage: "Unit value can't be {VALUE}, must be kg/litter/pcs",
+  //   },
+  // },
   quantity: {
     type: Number,
     required: true,
@@ -107,7 +64,7 @@ const productSchema = mongoose.Schema({
     type: String,
     required: true,
     enum: {
-      values: ["in-stock", "out-of-stock", "discontinued"],
+      values: ["in-stock", "out-of-stock", "low stock"],
       massage: "Status can't be {VALUE",
     },
   },
@@ -131,8 +88,44 @@ const productSchema = mongoose.Schema({
   ],
 });
 
+// middleware
+
+productSchema.pre("save", function (next) {
+  if (this.quantity === 0) {
+    this.status = "out-of-stack";
+  }
+  next()
+});
+
+const Product = mongoose.model("Product", productSchema);
+
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
+});
+
+// posting route
+
+app.post("/api/v1/product", async (req, res, next) => {
+  try {
+    const product = await new Product(req.body);
+    // if (product.quantity === 0) {
+    //   product.status = "out-of-stock";
+    // } else if (product.quantity <= 3) {
+    //   product.status = "low stock";
+    // }
+    const result = await product.save();
+    res.status(200).json({
+      status: "Success",
+      message: "Data inserted successfully",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Flied",
+      message: "Product insert Flied",
+      error: error.message,
+    });
+  }
 });
 
 module.exports = app;
